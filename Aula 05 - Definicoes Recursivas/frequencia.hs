decbc8b0e08f0e64025f363f4391a535ef103089
eudalio <tonnydallios@gmail.com>
2018-03-14 00:30:20 -0300

    activity_1 - class-05 - done

---
+++ Aula 05 - Definicoes Recursivas/frequencia.hs
@@ -0,0 +1,11 @@
+-- Defina a função frequencia :: a -> [a] -> Int tal que (frequencia x xs) devolve o número de ocorrências de x em u. Por exemplo,
+
+-- frequencia 2 [1,2,3,2,4] == 2
+
+-- frequencia 2 [2,1,2,3,2,4,2] == 4
+
+-- Considere os seguintes casos:
+
+frequencia a [] = 0
+frequencia a (x:xs) | x == a    = 1 + frequencia a xs
+                    | otherwise = frequencia a xs
