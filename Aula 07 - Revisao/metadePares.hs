fd366a1d80e80dbeb343bca7231a33b1339fe9ad
eudalio <tonnydallios@gmail.com>
2018-03-23 04:48:09 -0300

    activity_1 - class_07 - done

---
+++ Aula 07 - Revisao/metadePares.hs
@@ -0,0 +1,6 @@
+--Usando apenas funções da Biblioteca Prelude, escreva a função metadePares :: [Integer] -> [Integer] 
+--tal que (metadePares xs) devolve a lista dos elementos que são pares de xs divididos por 2. Por exemplo,
+
+--metadePares [0,-1,3,4,-16,3] == [0,2,-8]
+
+metadePares xs = [div x 2 | x <- (filter even xs)]
