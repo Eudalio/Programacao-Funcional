81c22fcf455ff5ff4e66ed1f8d10dae7129de062
eudalio <tonnydallios@gmail.com>
2018-03-24 16:28:31 -0300

    activity_3 - class_07 - done

---
+++ Aula 07 - Revisao/intercala.hs
@@ -0,0 +1,23 @@
+--Defina a função intercala :: a -> [a] -> [a] que intercala um elemento entre valores consecutivos
+
+--numa lista; se a lista tiver menos de dois valores deve ficar inalterada. Exemplos:
+
+{-> intercala 0 [ 1 . . 4 ]
+
+--[1 ,0 ,2 ,0 ,3 ,0 ,4]
+
+--> i n t e r c a l a ' , ' ” abcd ”
+
+--”a , b , c , d”
+
+--> i n t e r c a l a ' , ' ”a”
+
+--”a”
+
+--> intercala 1 [ ]
+
+--[]-}
+
+intercala a xs 
+    | (length xs) < 2 = []
+    | otherwise = (head xs):a:intercala a (tail xs)
