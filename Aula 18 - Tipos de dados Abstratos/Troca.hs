69a82a92b9d1f17e2422e1c6cffd67abbf7cda42
eudalio <tonnydallios@gmail.com>
2018-05-29 09:49:37 -0300

    One more exercise using queue structure

---
+++ Aula 18 - Tipos de dados Abstratos/Troca.hs
@@ -0,0 +1,35 @@
+import Conjunto
+
+
+--troca xs ys = numero_trocas_permitidas
+
+
+
+troca xs ys = min (tamanho c3) (tamanho c4)
+ where
+ c1 = fromList xs
+ c2 = fromList ys
+ c3 = diferenca c1 c2
+ c4 = diferenca c2 c1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
